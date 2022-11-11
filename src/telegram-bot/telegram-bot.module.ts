ce0bcb2faea11ab0acb1d4ff2c2f17bf493c6d32
Erlan <i@erlansulaimanov.com>
2022-11-11 13:36:18 +0600

    clean:

--- src/telegram-bot/telegram-bot.module.ts
+++ src/telegram-bot/telegram-bot.module.ts
@@ -1,10 +1,10 @@
-import { CacheModule, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TelegramBotUpdate } from './telegram-bot.update';
 import { RegistrationForEventScene } from './scenes/registration-for-event.scene';
 import { ConfigModule } from '../config/config.module';
 
 @Module({
   providers: [TelegramBotUpdate, RegistrationForEventScene],
-  imports: [CacheModule.register(), ConfigModule],
+  imports: [ConfigModule],
 })
 export class TelegramBotModule {}
